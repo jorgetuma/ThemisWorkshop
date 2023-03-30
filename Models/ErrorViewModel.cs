7c289dfdb30cc6e0a63e4615582b6b2c78765546
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-03-30 15:11:45 -0400

    reestructurando el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThwmisWorkshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
