b84e30190511d53f12ead523e67885df62fc8ff7
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-04-05 22:28:51 -0400

    Haciendo cambios a la clase cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,27 +2,26 @@ namespace ThemisWorkshop.Models
 {
    public class Cliente 
    {
-    public int id {get;set;}
+    public string id {get;set;}
     public string Nombre{get;set;}
     public string Apellido{get;set;}
     public string cedula{get;set;}
     public string lugarNacimiento{get;set;}
     public DateTime FechaNacimiento{get;set;}
-    public string nacionalidad{get;set;}
     public char sexo{get;set;}
     public string estadoCivil{get;set;}
     public string correo{get;set;}
     public string telefono{get;set;}
     public float credito{get;set;}
 
-    public Cliente(string Nombre, string Apellido, string cedula, string lugarNacimiento,DateTime FechaNacimiento, string nacionalidad,char sexo, string estadoCivil,string correo,string telefono)
+    public Cliente(string Nombre, string Apellido, string cedula, string lugarNacimiento,DateTime FechaNacimiento,char sexo, string estadoCivil,string correo,string telefono)
     {
+        this.id = System.Guid.NewGuid().ToString();
         this.Nombre = Nombre;
         this.Apellido = Apellido;
-        this.cedula =cedula;
+        this.cedula = cedula;
         this.lugarNacimiento = lugarNacimiento;
         this.FechaNacimiento = FechaNacimiento;
-        this.nacionalidad = nacionalidad;
         this.sexo = sexo;
         this.estadoCivil = estadoCivil;
         this.correo =correo;
