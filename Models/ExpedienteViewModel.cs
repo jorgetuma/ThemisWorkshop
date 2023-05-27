06eb8a957513639e9816e649f12c82854227425f
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-05-26 23:11:04 -0400

    implementando ExpedienteViewModel

---
+++ Models/ExpedienteViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ThemisWorkshop.Models
+{
+    public class ExpedienteViewModel
+    {
+        public Expediente Expediente { get; set; }
+
+        public bool EsMod { get; set; }
+
+        public ExpedienteViewModel(Expediente expediente,bool esMod) 
+        {
+            this.Expediente = expediente;
+            this.EsMod = esMod;
+        }
+    }
+}
