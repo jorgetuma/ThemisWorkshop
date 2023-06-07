d1999c6a57e313c10a6b17fdd5b60d96640c29bb
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-06-07 17:35:49 -0400

    Clase CitaViewModel

---
+++ Models/CitaViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ThemisWorkshop.Models
+{
+    public class CitaViewModel
+    {
+        public Cita? Cita;
+
+       public Cliente? Cliente { get; set; }
+
+        public bool EsMod { get; set; }
+
+        public CitaViewModel(Cita? cita,Cliente? cliente, bool esmod) 
+        { 
+            this.Cita = cita;
+            this.Cliente = cliente;
+            this.EsMod = esmod;
+        }
+    }
+}
