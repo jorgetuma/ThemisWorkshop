1a537d4d106edeaa52ffaa15cb11d7da000b9df1
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-07-03 19:23:24 -0400

    Controlador Sesion

---
+++ Controllers/SesionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThemisWorkshop.Models;
+
+namespace ThemisWorkshop.Controllers
+{
+    public class SesionController : Controller
+    {
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        {
+            SesionViewModel model = new SesionViewModel(" "," ");
+            return View("IniciarSesion",model);
+        }
+    }
+}
---
+++ Models/SesionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThemisWorkshop.Models
+{
+    public class SesionViewModel
+    {
+        [Required(ErrorMessage = "El campo Nombre de usuario es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo Nombre de usuario debe tener una longitud máxima de 50 caracteres")]
+        public string User { get; set; }
+
+        [Required(ErrorMessage = "El campo contraseña es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo contraseña debe tener una longitud máxima de 50 caracteres")]
+        public string Pass { get; set; }
+
+        public SesionViewModel(string user,string pass) 
+        { 
+            this.User = user;
+            this.Pass = pass;
+        }
+    }
+}
